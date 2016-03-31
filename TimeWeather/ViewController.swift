3491a51c20c4ebe38947304e9f869162039d447f
hanzhuzi <1754410821@qq.com>
2016-03-31 22:40:46 +0800

    commit project
    提交新工程项目文件

---
+++ TimeWeather/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  TimeWeather
+//
+//  Created by 寒竹子 on 16/3/31.
+//  Copyright © 2016年 X.R. All rights reserved.
+//
+
+/**
+ *  时光天气
+ *  by X.R
+ */
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
