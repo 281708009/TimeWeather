3bb17837264293a7afd7a77045c526f8208e6adc
apple <1754410821@qq.com>
2016-04-12 22:44:57 +0800

    commit changed
    提交修改

---
+++ TimeWeather/Classes/Macros/TimeWeatherConfig.h
@@ -0,0 +1,19 @@
+//
+//  TimeWeatherConfig.h
+//  TimeWeather
+//
+//  Created by xuran on 16/4/12.
+//  Copyright © 2016年 X.R. All rights reserved.
+//
+
+#ifndef TimeWeatherConfig_h
+#define TimeWeatherConfig_h
+
+// 获取城市代码
+// cityname : ""
+#define GET_CITY_CODE_URL  @"http://apistore.baidu.com/microservice/cityinfo?"
+// 获取某个城市的天气信息
+// cityid : ""
+#define GET_CITY_WEATHER_INFO_URL @"http://apistore.baidu.com/microservice/weather?"
+
+#endif /* TimeWeatherConfig_h */
---
+++ TimeWeather/TimeWeather-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  TimeWeather-Bridging-Header.h
+//  TimeWeather
+//
+//  Created by xuran on 16/4/12.
+//  Copyright © 2016年 X.R. All rights reserved.
+//
+
+#ifndef TimeWeather_Bridging_Header_h
+#define TimeWeather_Bridging_Header_h
+
+#import "TimeWeatherConfig.h"
+
+#endif /* TimeWeather_Bridging_Header_h */
